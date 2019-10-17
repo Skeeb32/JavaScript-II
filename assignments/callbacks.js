bc65ba525067a64cac7fb212448c3a12e90666ee
Skeeb Khan <skeebkhan@Skeebs-MacBook-Pro.local>
2019-10-17 13:46:28 -0700

    UPDATED

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -4,59 +4,53 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 function callback(arg) {
   console.log(arg);
 }
+
 /* 
   // GIVEN THIS PROBLEM:
-
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
   // SOLUTION:
-
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
-
   // NOTES ON THE SOLUTION:
-
   // firstItem is a higher order function.
   // It expects a callback (referred to as `cb`) as its second argument.
   // To test our solution, we can use the given `items` array and a variety of callbacks.
   // Note how callbacks can be declared separately, or inlined.
-
   // TEST 1 (inlined callback):
-
   const test1 = firstItem(items, item => `I love my ${item}!`);
   console.log(test1); // "I love my Pencil!"
-
   // TEST 2 (declaring callback before hand):
-
   function logExorbitantPrice(article) {
     return `this ${article} is worth a million dollars!`;
   };
-
   const test2 = firstItem(items, logExorbitantPrice);
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
+
+
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
-}
-getLength(items, callback);
-// console.log(getLength2);
+} 
+getLength(items, (leng) => {
+  console.log(leng);
+})
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr[arr.length - 1]);
+  cb(arr[arr.length -1])
 }
-// let sum = x + y;
-//return cb(sum);
-//}
-// sumNums(23, 23, callback);
-last()
+last(items, (lastIteam) => { 
+  console.log(lastIteam);
+})
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-  return cb(x + y);
+  return cb(x, y);
 }
 const add = (x, y) => {
   return x + y;
@@ -65,17 +59,12 @@ console.log(sumNums(2, 4, add));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  // let sum = x + y;
-  //return cb(return);
-  //}
-  // sumNums(23, 23, callback);
-  return cb(x * y);
+  return cb(x, y);
 }
 const multiply = (x, y) => {
   return x * y;
 }
 console.log(sumNums(2, 4, multiply));
-
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
